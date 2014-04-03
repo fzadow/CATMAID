a30a1d4993660b9e828491b2859f5d53efad53d8
fzadow <felix@von-zadow.de>
2014-04-03 15:45:10 +0200

    hide Image data slider

--- django/applications/catmaid/static/tilelayercontrol.js
+++ django/applications/catmaid/static/tilelayercontrol.js
@@ -42,47 +42,47 @@ function TilelayerControl( stack )
     $(view).empty()
 
     // Add slider for each layer
-    for(var key in layers)
-    {
-
-      var container = document.createElement("div");
-
-      var setOpac = function ( val )
-      {
-        self.setOpacity( this.idd, val );
-        stack.redraw();
-        return;
-      }
-
-      // Make layer re-evaluate it's opacity
-      layers[key].setOpacity(layers[key].getOpacity());
-
-      var slider = new Slider(
-          SLIDER_HORIZONTAL,
-          false,
-          1,
-          100,
-          100,
-          layers[key].getOpacity() * 100,
-          setOpac );
-
-      slider.idd = key;
-      container.setAttribute("id", key + "-container");
-      container.setAttribute("class", "layerControl");
-
-      var layer_name = layers[key].getLayerName ? layers[key].getLayerName() : key;
-      container.appendChild(document.createElement("strong").appendChild(
-          document.createTextNode(layer_name)));
-      container.appendChild( slider.getView() );
-
-      // A clearing div is needed, because sliders are usually floating.
-      // See i.e. http://stackoverflow.com/questions/14758932
-      var clearing = document.createElement('div');
-      clearing.setAttribute('class', 'clear');
-      container.appendChild(clearing);
-
-      view.appendChild(container);
-    }
+//    for(var key in layers)
+//    {
+//
+//      var container = document.createElement("div");
+//
+//      var setOpac = function ( val )
+//      {
+//        self.setOpacity( this.idd, val );
+//        stack.redraw();
+//        return;
+//      }
+//
+//      // Make layer re-evaluate it's opacity
+//      layers[key].setOpacity(layers[key].getOpacity());
+//
+//      var slider = new Slider(
+//          SLIDER_HORIZONTAL,
+//          false,
+//          1,
+//          100,
+//          100,
+//          layers[key].getOpacity() * 100,
+//          setOpac );
+//
+//      slider.idd = key;
+//      container.setAttribute("id", key + "-container");
+//      container.setAttribute("class", "layerControl");
+//
+//      var layer_name = layers[key].getLayerName ? layers[key].getLayerName() : key;
+//      container.appendChild(document.createElement("strong").appendChild(
+//          document.createTextNode(layer_name)));
+//      container.appendChild( slider.getView() );
+//
+//      // A clearing div is needed, because sliders are usually floating.
+//      // See i.e. http://stackoverflow.com/questions/14758932
+//      var clearing = document.createElement('div');
+//      clearing.setAttribute('class', 'clear');
+//      container.appendChild(clearing);
+//
+//      view.appendChild(container);
+//    }
   };
 
   var view = document.createElement( "div" );
